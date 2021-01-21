e18430a7f21c1557eb24f593f9c0072830b5847e
0xE2 <stub@gmail.com>
2021-01-21 10:41:52 +0200

    Rewrite for PyCryptodome

--- unscrambler.py
+++ unscrambler.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 
-from Crypto.Cipher.DES3 import DES3Cipher
-from codecs import decode
+import Crypto.Cipher.DES3 as DES3
 from sys import argv
 
 ciphertext = argv[1]
 
-key_signed_byte_array = [61, 22, 11, 57, 110, 89, -20, -1, 0, 99, 111, -120, 55, 4, -9, 10, 11, 45, 71, -89, 21, -99, 54, 51]
-key_byte_array = [ b & 0xff for b in key_signed_byte_array ]
-key = bytes(key_byte_array)
+teamcity_3DES_key = "3d160b396e59ecff00636f883704f70a0b2d47a7159d3633"
+key = bytearray.fromhex(teamcity_3DES_key)
 
 des3_ciphertext = ciphertext.lstrip('zxx')
-des3_cipherbytes = decode(des3_ciphertext, 'hex')
+des3_cipherbytes = bytearray.fromhex(des3_ciphertext)
 
-cipher = DES3Cipher(key)
+cipher = DES3.new(key, DES3.MODE_ECB)
 
 plaintext_padded = cipher.decrypt(des3_cipherbytes)
-
 plaintext = plaintext_padded[:-plaintext_padded[-1]]
 
 print(plaintext.decode('utf-8'))
